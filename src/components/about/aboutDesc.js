cd1341158251fd2f7484e44acdc8e5efd1bbb9df
Ankit Kumar <ankitkumarrockingstar@gmail.com>
2026-03-22 15:55:04 +0530

    BSc Data added

--- src/components/about/aboutDesc.js
+++ src/components/about/aboutDesc.js
@@ -5,6 +5,7 @@ import ContactCard from '../contact/contactCard';
 import Tech from './tech';
 import bmrc from '../../images/about/bmrc.png';
 import ohio from '../../images/about/ohio.png';
+import narayana from '../../images/about/narayana.png';
 
 function AboutDesc() {
   return (
@@ -23,26 +24,34 @@ function AboutDesc() {
                         lice of Life :
                     </div>
                     <div className="subbu">
-                        ubbu...
+                        ubhashini
                     </div>
                 </div>
             </div>
             <p className="aboutBody">
                 Hello, nice to meet you!<br />
                 As a dedicated <b>B.Sc. Nursing student</b>, I am eager to enhance my clinical skills and contribute to <b>patient care excellence</b>. I am committed to upholding high standards of compassionate care and aim to gain hands-on experience in a dynamic healthcare environment. <br />My focus is on <b>refining critical thinking, developing adaptability, and collaborating with interdisciplinary teams</b> to impact patient outcomes positively. By embracing evidence-based practices, I strive to contribute to the advancement of healthcare practices.<br /><br />
-                <b>Languages: </b>English (Profficient), Hindi (Native)
+                <b>Languages: </b>English (Profficient), Hindi (Native), Bengali (Basic)
             </p>
+            <div className='downloadResume'>
+                <button>
+                    <a href='https://drive.google.com/file/d/170M_3U2DVNkaiA7pisBOrnHyzbmZjScL/view?usp=drivesdk' target="_blank" rel="noreferrer">
+                        Resume
+                    </a>
+                </button>
+            </div>
         </div>
     </div>
     <div className="aboutSliderOuter">
     <div className="aboutSlider">
-        <ContactCard link='https://kolkatainstituteofnursing.in/b-sc-nursing/' site='Kolkata Institute of Nursing' points={['3rd Year B.Sc. Student', '2021-2025', '2x 3-Months Trainee']} />
+        <ContactCard link='https://share.google/TNBuqyjhovsMQ0gGl' site='R.K College of Nursing' points={['Final Year B.Sc. Student', '2021-2025', '3x 3-Months Trainee']} />
         {/* <ContactCard link='https://www.surajkundinternational.com/' site='Surajkund International School' points={['2020 Passout', 'PCB Student' ]} /> */}
     </div></div>
     <div className="aboutSliderOuter">
     <div className="aboutSlider">
-        <Tech icon={ohio} field="OHIO Hospital" content="Worked as Trainee for 3 months in 2023. Trained under Emergency, ICU, General and Swasth-Sathi wards." />
+        <Tech icon={narayana} field="Narayana Hospital" content="Completed a 3-month clinical training (2025) across ICU, General, and Emergency wards as a Trainee." />
         <Tech icon={bmrc} field="BMRC Hospital" content="Trained under ICCU and CCU wards over a span of 3 months (2024) as Trainee." />
+        <Tech icon={ohio} field="OHIO Hospital" content="Worked as Trainee for 3 months in 2023. Trained under Emergency, ICU, General and Swasth-Sathi wards." />
         {/* <Tech icon="fa-solid fa-robot" field="AI (ML & DL)" content="Have Deep insights of various topics related to ML & DL. Always have dived deep into the advanced concepts and mathematical point of view of DL, which explains the WHY and HOW of the concepts." /> */}
     </div></div>
     </div>
