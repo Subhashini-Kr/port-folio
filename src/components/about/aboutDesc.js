be658e713e95f8f8cfd44410af7bd85cd8829f3c
Ankit Kumar <ankitkumarrockingstar@gmail.com>
2026-03-28 01:36:47 +0530

    Bsc Nursing Graduate

--- src/components/about/aboutDesc.js
+++ src/components/about/aboutDesc.js
@@ -30,12 +30,12 @@ function AboutDesc() {
             </div>
             <p className="aboutBody">
                 Hello, nice to meet you!<br />
-                As a dedicated <b>B.Sc. Nursing student</b>, I am eager to enhance my clinical skills and contribute to <b>patient care excellence</b>. I am committed to upholding high standards of compassionate care and aim to gain hands-on experience in a dynamic healthcare environment. <br />My focus is on <b>refining critical thinking, developing adaptability, and collaborating with interdisciplinary teams</b> to impact patient outcomes positively. By embracing evidence-based practices, I strive to contribute to the advancement of healthcare practices.<br /><br />
+                As a dedicated <b>B.Sc. Nursing Graduate</b>, I am eager to enhance my clinical skills and contribute to <b>patient care excellence</b>. I am committed to upholding high standards of compassionate care and aim to gain hands-on experience in a dynamic healthcare environment. <br />My focus is on <b>refining critical thinking, developing adaptability, and collaborating with interdisciplinary teams</b> to impact patient outcomes positively. By embracing evidence-based practices, I strive to contribute to the advancement of healthcare practices.<br /><br />
                 <b>Languages: </b>English (Profficient), Hindi (Native), Bengali (Basic)
             </p>
             <div className='downloadResume'>
                 <button>
-                    <a href='https://drive.google.com/file/d/170M_3U2DVNkaiA7pisBOrnHyzbmZjScL/view?usp=drivesdk' target="_blank" rel="noreferrer">
+                    <a href='https://drive.google.com/file/d/1TATayXf1RTiJpFNMTg6IpUXufG8nfxiu/view' target="_blank" rel="noreferrer">
                         Resume
                     </a>
                 </button>
@@ -44,12 +44,14 @@ function AboutDesc() {
     </div>
     <div className="aboutSliderOuter">
     <div className="aboutSlider">
-        <ContactCard link='https://share.google/TNBuqyjhovsMQ0gGl' site='R.K College of Nursing' points={['Final Year B.Sc. Student', '2021-2025', '3x 3-Months Trainee']} />
+        <ContactCard link='https://share.google/TNBuqyjhovsMQ0gGl' site='R.K College of Nursing' points={['B.Sc. Nursing Graduate', '2021-2025', '3x 3-Months Trainee']} />
         {/* <ContactCard link='https://www.surajkundinternational.com/' site='Surajkund International School' points={['2020 Passout', 'PCB Student' ]} /> */}
     </div></div>
     <div className="aboutSliderOuter">
     <div className="aboutSlider">
-        <Tech icon={narayana} field="Narayana Hospital" content="Completed a 3-month clinical training (2025) across ICU, General, and Emergency wards as a Trainee." />
+        <a href='https://drive.google.com/file/d/1ZmU0O6j84PmctfjRhBM3u5vtU5lt9_pO/view' target="_blank" rel="noreferrer">
+            <Tech icon={narayana} field="Narayana Hospital" content="Completed a 3-month clinical training (2025) across ICU, General, and Emergency wards as a Trainee." />
+        </a>
         <Tech icon={bmrc} field="BMRC Hospital" content="Trained under ICCU and CCU wards over a span of 3 months (2024) as Trainee." />
         <Tech icon={ohio} field="OHIO Hospital" content="Worked as Trainee for 3 months in 2023. Trained under Emergency, ICU, General and Swasth-Sathi wards." />
         {/* <Tech icon="fa-solid fa-robot" field="AI (ML & DL)" content="Have Deep insights of various topics related to ML & DL. Always have dived deep into the advanced concepts and mathematical point of view of DL, which explains the WHY and HOW of the concepts." /> */}
--- src/components/home/posts.js
+++ src/components/home/posts.js
@@ -6,8 +6,8 @@ function Posts() {
     <div className='postsSection'>
         <h1 className="constant">I am a</h1>
         <div className="posts">
-        <h1 className="child1">Under-Training Nurse...</h1>
-        <h1 className="child2">B.Sc. Nursing Student...</h1>
+        {/* <h1 className="child1">Under-Training Nurse...</h1> */}
+        <h1 className="child2">B.Sc. Nursing Graduate...</h1>
         <h1 className="child3">Sketch Artist !!</h1>
         {/* <h1 className="child4">IIITian !!</h1> */}
     </div>
